8323bea4af6ed977d0467466a7094e2d86d256b3
Subham Sahu <about.subhamsahu@gmail.com>
2022-09-24 10:45:34 +0530

    24th Sept 2022

--- Section-9/105-Spread-Operator/script.js
+++ Section-9/105-Spread-Operator/script.js
@@ -36,4 +36,63 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    return console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} & ${ing3}`
+    );
+  },
 };
+
+// Arrays
+
+const arr = [7, 8, 9];
+const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArray);
+
+// Spread operator
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, "Gnocchi"];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log(mainMenuCopy);
+
+// Join 2 arrays
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+
+// Spread operator works on all Iterables; like arrays, strings, maps, sets but not for objects
+
+const str = "Jonas";
+const letters = [...str, " ", "S."];
+console.log(letters);
+console.log(...str);
+
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt("Let's make pasta! Ingredient 2?"),
+//   prompt("Let's make pasta! Ingredient 3?"),
+// ];
+
+// console.log(ingredients);
+
+// restaurant.orderPasta(...ingredients);
+
+// Objects
+
+const newRestaurant = {
+  foundingYear: 1998,
+  ...restaurant,
+  founder: "Guiseppe",
+};
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = "Ristorante Roma";
+console.log("Prev name:", restaurant.name, "\nNew name:", restaurantCopy.name);
