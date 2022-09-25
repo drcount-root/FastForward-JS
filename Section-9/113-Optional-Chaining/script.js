8a7c5356180c9f2f83619102f868ad5098607beb
Subham Sahu <about.subhamsahu@gmail.com>
2022-09-25 19:57:19 +0530

    25th Sept 2022

---
+++ Section-9/113-Optional-Chaining/script.js
@@ -0,0 +1,83 @@
+"use strict";
+
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  //   [`day-${2 + 4}`]: {
+  //     open: 0,
+  //     close: 24,
+  //   },
+  [weekdays[5]]: {
+    open: 0,
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  // ES6 Enhanced Object Literal for properties
+  openingHours,
+
+  // ES6 Enhanced Object Literal for methods
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = "20:00", address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    return console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} & ${ing3}`
+    );
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+// with optional chaining (?.)
+console.log(restaurant.openingHours.mon?.open);
+//  if restaurant.openingHours.mon exists then the open property will be read from there
+// In case not present, then undefined will be returned
+
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+for (const day of days) {
+  console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? "closed";
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? "Method doesn't exists!");
+
+console.log(restaurant.orderRisotto?.(0, 1) ?? "Method doesn't exists!");
+
+// Arrays
+
+const users = [{ name: "Jonas", email: "hello@jonas.io" }];
+console.log(users[0]?.email ?? "Users array empty");
+
+console.log(users[0]?.phone ?? "Users array empty");
