ce05bbc28b08187a4546d7b96ad3e7023686a24f
Subham Sahu <about.subhamsahu@gmail.com>
2022-09-25 08:49:43 +0530

    25th Sept 2022

---
+++ Section-9/108-Nullish-Coalescing-Operator/script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    return console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} & ${ing3}`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+// restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish Coalescing operator
+// Nullish : null and undefined (NOT 0 or '')
+const guestsCorrect = restaurant.numGuests ?? 10;
+// Only if the restaurant.numGuests is null or undefined then the second operand would be executed and returned.
+console.log(guestsCorrect);
+// Suppose restaurant.numGuests = 0 and it's not a nullish value, so the evaluation here is short-circuited, and the immediately the first non-nullish value is returned.
